0e90fd901da07eebc86e98d7a489dfa84cad90e7
Wendley Paulo de França <wendley.franca@ccc.ufcg.edu.br>
2016-11-20 01:51:17 -0300

    criando UI para o login , e implementando login via gmail, ccc ...

--- app/src/main/java/com/povmt/les/povmtprojetopiloto/activities/MainActivity.java
+++ app/src/main/java/com/povmt/les/povmtprojetopiloto/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.povmt.les.povmtprojetopiloto;
+package com.povmt.les.povmtprojetopiloto.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.povmt.les.povmtprojetopiloto.R;
 
 public class MainActivity extends AppCompatActivity {
 
