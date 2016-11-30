3fc8e48380da6fb5933555f0a30bce697c200973
ebarross <2974825e>
2016-11-30 09:52:05 -0300

    Implementaçao do historico de atividades

--- app/src/main/java/com/povmt/les/povmtprojetopiloto/Models/ActivityItem.java
+++ app/src/main/java/com/povmt/les/povmtprojetopiloto/Models/ActivityItem.java
@@ -106,6 +106,28 @@ public class ActivityItem implements Serializable, Comparable<ActivityItem>{
         return false;
     }
 
+    /*
+        Verifica se a atividade é das ultimas duas semanas.
+     */
+    @Exclude
+    public boolean isActivityTwoLastWeeks() {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(sdf.parse(updatedAt));
+            Calendar current = Calendar.getInstance();
+            int weekOfActivity = cal.get(Calendar.WEEK_OF_YEAR);
+
+            if (weekOfActivity == current.get(Calendar.WEEK_OF_YEAR) - 1 || weekOfActivity == current.get(Calendar.WEEK_OF_YEAR) - 2) {
+                return true;
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     @Exclude
     public void addNewInvestedTime(InvestedTimeItem  investedTimeItem){
         totalInvestedTime += investedTimeItem.getTime();
